0dc967b9e4e7887b0a5a1a95a44669a20699a363
astrowjz <35196915+astro-wjz@users.noreply.github.com>
2021-04-20 20:14:08 +0800

    Add files via upload

--- py_plot_cyl_densslice/run_cyl_dens.sh
+++ py_plot_cyl_densslice/run_cyl_dens.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
 
 ######## Description ########
-# Program for visualizing hdf5 result files from Osiris cyl modes code.
+# Program for vasualizing hdf5 result files from Osiris cyl modes decomposition code, 
+# and combine a slice along z-driction at any angle. Especially for plotting density files.
 
 ######## How to use #########
-# Two input parameters in file "run_cyl_ori.sh"
+# Seven input parameters in file "run_cyl_dens.sh"
 # Users should modify them before running
-# workpath: path to folder "FLD", which contains original h5 files
+# workpath: path to folder "DENSITY", which contains original h5 files
 # phi: angle of the slice along z axis
+# rmax: the real max length of r direction
+# zmin: the real min position of z direction
+# zmax: the real max position of z direction
+# npr: total amount of points along r direciton
+# npz: total amount of points along z direction
 
 ######## Results #########
-# PNG files will be output to folder "RESULTS" which has same level with "FLD"
+# Images and movies will be output to folder "RESULTS" which has same level with "DNESITY"
 
 workpath="/Users/astrowjz/Desktop/read_h5/trans2silo/DENSITY"
 phi=0.0
